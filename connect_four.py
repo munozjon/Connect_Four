817eadf8034f5e61061c7af9a0e64cc16a586bc9
Jonathan Munoz <munozjon98@gmail.com>
2023-11-18 11:20:13 -0500

    added an array for rows and columns to use throughout the game's functions.

--- connect_four.py
+++ connect_four.py
@@ -1,1 +1,9 @@
+import numpy as np
+
+# main board rows
+# creates an array of the rows for the board, with 6 rows of 7 values (all zeroes initially)
+main_rows_arr = np.zeros((6, 7), int)
+
+
+
 print("Welcome to Connect Four!")
